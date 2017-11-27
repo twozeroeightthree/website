31a2b91812b60297ee0ad82811289184c27c8f8a
haztecaso <adrianlattes@gmail.com>
2017-11-27 20:13:48 +0100

    rm _site

--- _site/assets/home.js
+++
@@ -1,7 +0,0 @@
-$("#slideshow .element").each(function(){
-  $(this).on("mouseenter", function(){
-    $(this).find(".description").css("visibility","visible");
-  }).on("mouseleave", function(){
-    $(this).find(".description").css("visibility","hidden");
-  });
-});
