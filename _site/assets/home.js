68b32bc622322f404c29251e7d1b994cfe48b201
haztecaso <adrianlattes@gmail.com>
2017-11-27 22:02:11 +0100

    Compile script...

--- _site/assets/home.js
+++ _site/assets/home.js
@@ -5,4 +5,16 @@ $("#slideshow .element").each(function(){
     $(this).find(".description").css("visibility","hidden");
   });
 });
-$("#slideshow").css("width", $(window).width()-450-25+"px")
+$("#slideshow").css("width", $(window).width()-505+"px")
+
+var t = 1500;
+
+function hide(){
+  $("#description").fadeOut(t);
+  $("#slideshow").animate({left:"100px"},t);
+  resizeSlideshow()
+}
+
+function resizeSlideshow(){
+  $("#slideshow").animate({width:$(window).width()-100},t);
+}
--- compile.sh
+++ compile.sh
@@ -3,3 +3,6 @@ jekyll build
 rm -rf ~/Sites/
 cp -rf ./_site/ ~/Sites/
 chmod -R 777 ~/Sites/
+git add .
+git commit -m "Compile script..."
+git push
