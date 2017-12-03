5e08fa1193e979a7d474840f37f8a67154a117a2
haztecaso <adrianlattes@gmail.com>
2017-12-03 23:12:45 +0100

    2017-12-03_23:12

--- assets/home.js
+++ assets/home.js
@@ -33,11 +33,18 @@ $( window ).resize(function() {
 });
 
 function updateBlocks(){
-
   var n = Math.ceil(elements / delta);
   var m = $("#indicators .indicator").length;
-
-  $("#indicators .indicator").last().remove();
+  if (m>n){
+    for (var i=0;i<m-n;i+=1){
+      $("#indicators .indicator").last().remove();
+    }
+  }
+  else if (m<n) {
+    for (var i=0;i<m-n;i+=1){
+      $("#indicators").last().append("<div class="indicator"></div>");
+    }
+  }
 }
 
 function goto(n){
