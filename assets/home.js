565b0fccbed3f0dcb96934d7628f391544f047ed
haztecaso <adrianlattes@gmail.com>
2017-12-03 20:19:35 +0100

    Compile script...

--- assets/home.js
+++ assets/home.js
@@ -19,7 +19,7 @@ function hide(){
 
 
 function goto(n){
-  var pos = $("#slideshow .element").eq(n).position().left;
+  var pos = $("#slideshow .element").eq(n).offset().left;
   $("#slideshow").animate({
     scrollLeft: pos
   }, t);
